568f2d3dc42cd341e6e303f02f7ab6a35f07002c
Jacob Smith <jacob101607@gmail.com>
2024-12-12 22:08:14 -0800

    Solve linear equation manually without Z3

--- src/year_2024/day_13.rs
+++ src/year_2024/day_13.rs
@@ -1,175 +1,81 @@
-use std::{cmp::Reverse, collections::BinaryHeap, fmt::Binary, iter::Rev};
-
-use fxhash::FxHashSet;
-use z3::ast::Ast;
 
 use crate::utils::Point2D;
 
-fn parse_int(chars: &mut impl Iterator<Item = char>) -> isize {
-    let mut value = 0;
-    while let Some(c) = chars.next() {
-        if !c.is_alphanumeric() {
-            break;
-        }
-
-        value = (value * 10) + ((c as isize) - '0' as isize);
+fn parse_int(bytes: &[u8], index: &mut usize) -> isize {
+    let mut sign = 1;
+    if bytes[*index] == b'-' {
+        sign = -1;
+        *index += 1;
     }
 
-    value
+    let mut val = 0;
+    while *index < bytes.len() && bytes[*index] >= b'0' && bytes[*index] <= b'9' {
+        val = (val * 10) + ((bytes[*index] - b'0') as isize);
+        *index += 1;
+    }
+
+    val * sign
 }
 
-fn find_min_cost(destination: Point2D, button_a: Point2D, button_b: Point2D) -> u64 {
-    let mut queue = BinaryHeap::new();
-    queue.push(Reverse((0, Point2D::new(0, 0))));
+fn parse_machine_description<'a, const OFFSET: isize>(lines: &mut impl Iterator<Item = &'a str>) -> Option<(Point2D, Point2D, Point2D)> {
+    let Some(line_1) = lines.next() else { return None; };
+    let Some(line_2) = lines.next() else { return None; };
+    let Some(line_3) = lines.next() else { return None; };
+    lines.next();
 
-    let mut visited = FxHashSet::default();
-    while let Some(Reverse((cost, current_pos))) = queue.pop() {
-        if current_pos == destination {
-            return cost;
-        }
+    let point_a = {
+        let bytes = line_1.as_bytes();
+        let mut index = b"Button A: X+".len();
+        let x = parse_int(bytes, &mut index);
 
-        if current_pos.x_index() > destination.x_index() ||
-           current_pos.y_index() > destination.y_index() {
-            
-            continue;
-        }
+        index += b", Y+".len();
+        let y = parse_int(bytes, &mut index);
+        Point2D::new(x, y)
+    };
 
-        if visited.contains(&current_pos) {
-            continue;
-        }
+    let point_b = {
+        let bytes = line_2.as_bytes();
+        let mut index = b"Button B: X+".len();
+        let x = parse_int(bytes, &mut index);
 
-        visited.insert(current_pos);
-        queue.push(Reverse((cost + 3, current_pos + button_a)));
-        queue.push(Reverse((cost + 1, current_pos + button_b)));
-    }
+        index += b", Y+".len();
+        let y = parse_int(bytes, &mut index);
+        Point2D::new(x, y)
+    };
 
-    0
+    let destination = {
+        let bytes = line_3.as_bytes();
+        let mut index = b"Prize: X=".len();
+        let x = parse_int(bytes, &mut index) + OFFSET;
+
+        index += b", Y=".len();
+        let y = parse_int(bytes, &mut index) + OFFSET;
+        Point2D::new(x, y)
+    };
+
+    Some((point_a, point_b, destination))
 }
 
-use crate::utils::z3::*;
-use z3::{Config, Context, Optimize, SatResult, ast::Int};
+fn find_min_cost<const OFFSET: isize>(input: &str) -> u64 {
+    let mut lines = input.trim().lines();
+    let mut total_cost = 0;
+    while let Some((button_a, button_b, destination)) = parse_machine_description::<OFFSET>(&mut lines) {
+        let b = (destination.y * button_a.x - destination.x * button_a.y) / (button_b.y * button_a.x - button_b.x * button_a.y);
+        let a = (destination.x - b * button_b.x) / button_a.x;
+        if (button_a.x * a + button_b.x * b) == destination.x &&
+           (button_a.y * a + button_b.y * b) == destination.y {
 
-fn find_min_cost_z3(destination: Point2D, button_a: Point2D, button_b: Point2D) -> u64 {
-    let config = Config::new();
-    let context = Context::new(&config);
-    let solver = Optimize::new(&context);
-
-    let one = Int::from_i64(&context, 1);
-    let three = Int::from_i64(&context, 3);
-
-    let d_x = Int::from_i64(&context, destination.x as i64);
-    let d_y = Int::from_i64(&context, destination.y as i64);
-
-    let a_x = Int::from_i64(&context, button_a.x as i64);
-    let a_y = Int::from_i64(&context, button_a.y as i64);
-    let a_count = Int::new_const(&context, "a_count");
-
-    let b_x = Int::from_i64(&context, button_b.x as i64);
-    let b_y = Int::from_i64(&context, button_b.y as i64);
-    let b_count = Int::new_const(&context, "b_count");
-
-    solver.assert(&((&a_x * &a_count) + (&b_x * &b_count))._eq(&d_x));
-    solver.assert(&((&a_y * &a_count) + (&b_y * &b_count))._eq(&d_y));
-
-    let cost = Int::new_const(&context, "cost");
-    solver.assert(&((&a_count * &three) + (&b_count * &one))._eq(&cost));
-    solver.minimize(&cost);
-
-    if SatResult::Sat == solver.check(&[]) {
-        let model = solver.get_model().unwrap();
-        model.eval(&cost, true).unwrap().as_i64().unwrap() as u64
-
-    } else {
-        0
+            total_cost += a * 3 + b;
+        }
     }
+
+    total_cost as u64
 }
 
 pub fn part1(input: &str) -> u64 {
-    let mut lines = input.trim().lines();
-    let mut cost = 0;
-    loop {
-        let Some(line_1) = lines.next() else { break; };
-        let line_2 = lines.next().unwrap();
-        let line_3 = lines.next().unwrap();
-
-        let button_a = {
-            let mut chars = line_1.chars().skip(12);
-            let x_mov = parse_int(&mut chars);
-            chars.next().unwrap();
-            chars.next().unwrap();
-            chars.next().unwrap();
-            let y_mov = parse_int(&mut chars);
-            Point2D::new(x_mov, y_mov)
-        };
-
-        let button_b = {
-            let mut chars = line_2.chars().skip(12);
-            let x_mov = parse_int(&mut chars);
-            chars.next().unwrap();
-            chars.next().unwrap();
-            chars.next().unwrap();
-            let y_mov = parse_int(&mut chars);
-            Point2D::new(x_mov, y_mov)
-        };
-
-        let prize = {
-            let mut chars = line_3.chars().skip(9);
-            let x_pos = parse_int(&mut chars);
-            chars.next().unwrap();
-            chars.next().unwrap();
-            chars.next().unwrap();
-            let y_pos = parse_int(&mut chars);
-            Point2D::new(x_pos, y_pos)
-        };
-
-        cost += find_min_cost(prize, button_a, button_b);
-        lines.next();
-    }
-    
-    cost
+    find_min_cost::<0>(input)
 }
 
 pub fn part2(input: &str) -> u64 {
-    let mut lines = input.trim().lines();
-    let mut cost = 0;
-    loop {
-        let Some(line_1) = lines.next() else { break; };
-        let line_2 = lines.next().unwrap();
-        let line_3 = lines.next().unwrap();
-
-        let button_a = {
-            let mut chars = line_1.chars().skip(12);
-            let x_mov = parse_int(&mut chars);
-            chars.next().unwrap();
-            chars.next().unwrap();
-            chars.next().unwrap();
-            let y_mov = parse_int(&mut chars);
-            Point2D::new(x_mov, y_mov)
-        };
-
-        let button_b = {
-            let mut chars = line_2.chars().skip(12);
-            let x_mov = parse_int(&mut chars);
-            chars.next().unwrap();
-            chars.next().unwrap();
-            chars.next().unwrap();
-            let y_mov = parse_int(&mut chars);
-            Point2D::new(x_mov, y_mov)
-        };
-
-        let prize = {
-            let mut chars = line_3.chars().skip(9);
-            let x_pos = parse_int(&mut chars) + 10000000000000;
-            chars.next().unwrap();
-            chars.next().unwrap();
-            chars.next().unwrap();
-            let y_pos = parse_int(&mut chars) + 10000000000000;
-            Point2D::new(x_pos, y_pos)
-        };
-
-        cost += find_min_cost_z3(prize, button_a, button_b);
-        lines.next();
-    }
-    
-    cost
+    find_min_cost::<10000000000000>(input)
 }
