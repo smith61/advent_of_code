9fd2b0abd12170a3eedf9f18605df3db82793e55
Jacob Smith <jacob101607@yahoo.com>
2024-12-22 07:06:48 -0800

    Improve day 22 performance

--- src/year_2024/day_22.rs
+++ src/year_2024/day_22.rs
@@ -1,7 +1,5 @@
-use fxhash::{FxHashMap, FxHashSet};
 
 use crate::scaffold::InputParser;
-
 
 fn generate_next_number(current_number: u64) -> u64 {
     let mut number = current_number;
@@ -15,7 +13,6 @@ fn generate_next_number(current_number: u64) -> u64 {
 pub fn part1(mut input: InputParser) -> u64 {
     let mut result = 0;
     while let Some(mut number) = input.next_uint() {
-        
         for _ in 0..2000 {
             number = generate_next_number(number);
         }
@@ -27,55 +24,44 @@ pub fn part1(mut input: InputParser) -> u64 {
 }
 
 pub fn part2(mut input: InputParser) -> u64 {
-    let mut result = 0;
-    let mut prices_changes_all = Vec::new();
-    let mut sequences = FxHashSet::default();
-    while let Some(orig_number) = input.next_uint() {
-        let mut number = orig_number;
-        let mut previous_price = number % 10;
-        
-        let mut price_changes = Vec::new();
+    const SHIFT_VALUE: usize = 20;
+    const MASK_VALUE: usize = SHIFT_VALUE * SHIFT_VALUE * SHIFT_VALUE * SHIFT_VALUE;
 
-        let mut prices = Vec::new();
-        for _ in 0..2000 {
+    let mut current_max = 0;
+    let mut sequence_counts = vec![0u32; MASK_VALUE];
+    let mut visited = vec![0u32; MASK_VALUE];
+
+    let mut line_number = 1;
+    while let Some(mut number) = input.next_uint() {
+        let mut previous_price = (number % 10) as u32;
+        let mut current_sequence = 0;
+        for _ in 0..3 {
             number = generate_next_number(number);
-            let next_price = number % 10;
-            let diff = next_price as i64 - previous_price as i64;
-            price_changes.push(diff);
-            prices.push(next_price);
+            let next_price = (number % 10) as u32;
+            let diff = (next_price + 9 - previous_price) as usize;
             previous_price = next_price;
+            
+            current_sequence = ((current_sequence * SHIFT_VALUE) + diff) % MASK_VALUE;
         }
 
-        let mut change_set = FxHashMap::<Vec<i64>, u64>::default();
-        for i in 4..price_changes.len() {
-            let changes = &price_changes[(i - 4)..i];
-            if changes[0] == -2 && changes[1] == 1 && changes[2] == -1 && changes[3] == 3 {
-                //println!("[{}] {:?}: {}", orig_number, changes, prices[i - 1]);
-            }
+        for _ in 3..2000 {
+            number = generate_next_number(number);
+            let next_price = (number % 10) as u32;
+            let diff = (next_price + 9 - previous_price) as usize;
+            previous_price = next_price;
 
-            sequences.insert(changes.to_vec());
-            change_set.entry(changes.to_owned())
-                      .or_insert(prices[i - 1]);
-        }
+            current_sequence = ((current_sequence * SHIFT_VALUE) + diff) % MASK_VALUE;
+            if visited[current_sequence] != line_number {
+                assert!(visited[current_sequence] < line_number);
 
-        prices_changes_all.push(change_set);
-    }
-
-    for key in sequences {
-        let mut current_result = 0;
-        for other in &prices_changes_all {
-
-            if other.contains_key(&key) {
-                current_result += other.get(&key).unwrap();
+                visited[current_sequence] = line_number;
+                sequence_counts[current_sequence] += next_price;
+                current_max = current_max.max(sequence_counts[current_sequence]);
             }
         }
-        
 
-        if current_result > result {
-            //println!("{:?} = {}", key, current_result);
-            result = current_result;
-        }
+        line_number += 1;
     }
 
-    result
+    current_max as u64
 }
